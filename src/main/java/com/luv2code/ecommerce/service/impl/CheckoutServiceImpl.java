4f51ed76cec468a3b1422963a03fc5e3b952be5f
mahmoud-077 <mahmoudadel537@gmail.com>
2024-12-25 10:06:41 +0200

    convert http to https and integrare Stripe payment

--- src/main/java/com/luv2code/ecommerce/service/impl/CheckoutServiceImpl.java
+++ src/main/java/com/luv2code/ecommerce/service/impl/CheckoutServiceImpl.java
@@ -1,17 +1,22 @@
 package com.luv2code.ecommerce.service.impl;
 
 import com.luv2code.ecommerce.dao.CustomerRepository;
+import com.luv2code.ecommerce.dto.PaymentInfo;
 import com.luv2code.ecommerce.dto.Purchase;
 import com.luv2code.ecommerce.dto.PurchaseResponse;
 import com.luv2code.ecommerce.entity.Customer;
 import com.luv2code.ecommerce.entity.Order;
 import com.luv2code.ecommerce.entity.OrderItem;
 import com.luv2code.ecommerce.service.CheckoutService;
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import com.stripe.param.checkout.SessionCreateParams;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 
 @Service
 public class CheckoutServiceImpl implements CheckoutService {
@@ -19,8 +24,9 @@ public class CheckoutServiceImpl implements CheckoutService {
     private CustomerRepository customerRepository;
 
     @Autowired
-    public CheckoutServiceImpl(CustomerRepository customerRepository) {
+    public CheckoutServiceImpl(CustomerRepository customerRepository, @Value("${stripe.key.secret}") String secretKey) {
         this.customerRepository = customerRepository;
+        Stripe.apiKey = secretKey;
     }
 
     @Override
@@ -48,6 +54,21 @@ public class CheckoutServiceImpl implements CheckoutService {
 
         return new PurchaseResponse(orderTrackingNumber);
 
+    }
+
+    @Override
+    public PaymentIntent createPaymentIntent(PaymentInfo paymentInfo) throws StripeException {
+        List<String> paymentMethodTypes = new ArrayList<>();
+        paymentMethodTypes.add("card");
+
+        Map<String,Object> params = new HashMap<>();
+        params.put("amount",paymentInfo.getAmount());
+        params.put("currency",paymentInfo.getCurrency());
+        params.put("payment_method_types",paymentMethodTypes);
+        params.put("description","Luv2Shop Purchase");
+        params.put("receipt_email",paymentInfo.getReceiptEmail());
+
+        return PaymentIntent.create(params);
     }
 
     private String generateOrderTrackingNumber() {
